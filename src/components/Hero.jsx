7230010ee315403c526070e13983adf51c1f12d1
Alejandro Velez <velez.alejandro4@gmail.com>
2024-05-02 19:51:50 -0400

    added descriprion and fixed margin of hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,19 +6,22 @@ const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto mt-20 z-10">
       <div
-        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+        className={`${styles.paddingX} absolute inset-0 top-[20px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
+        <div>
+        <h1 className={`${styles.heroHeadText} text-white`}>
+          Hi, I'm <span className="text-[#915eff]">Alejandro</span>
+        </h1>
+        <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+          I am a aspiring software developer with a passion for learning.<br className="sm:block hidden"/> I develop projects using multiple technologies, here are some of my favorites!
+        </p>
+      </div>
       </div>
 
-      <div>
-        <h1 className={`${styles.heroHeadText} text-white`}>
-          Hi, My name is <span className="text-[#915eff]">Alejandro</span>
-        </h1>
-      </div>
     </section>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false);
 
   return (
-    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-30 bg-primary`}>
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
         <Link
           to="/"
@@ -25,7 +25,8 @@ const Navbar = () => {
           <span className='sm:block hidden'>| Velez</span>
           </p>
         </Link>
-        <ul className='list-none sm:flex-row gap-10'>
+
+        <ul className='list-none hidden md:flex sm:flex-row  gap-10'>
           {navLinks.map((link) => (
             <li
               key={link.id}
@@ -40,6 +41,7 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
+
         <div className='sm:hidden flex flex-1 justify-end items-center'>
           <img src={toggle? close : menu} alt='menu' className='w-[28px] h-[28px] object-contain cursor-pointer' onClick={() => setToggle(!toggle)}/>
           <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w[140px] z-10 rounded-xl`}>
