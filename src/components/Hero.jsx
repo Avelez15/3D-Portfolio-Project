91cbcfa0adf3a57ec657e5fbfb2e7dbf5c3d9107
Alejandro Velez <velez.alejandro4@gmail.com>
2024-05-03 19:50:55 -0400

    fixed positioning of computer 3d model

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 const Hero = () => {
   return (
-    <section className="relative w-full h-screen mx-auto mt-20 z-10">
+    <section className="relative w-full h-screen mx-auto mt-20 mb-10 z-10">
       <div
         className={`${styles.paddingX} absolute inset-0 top-[20px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
@@ -17,7 +17,7 @@ const Hero = () => {
             Hi, I'm <span className="text-[#915eff]">Alejandro</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I am a aspiring software developer with a passion for learning.
+            I am an aspiring software developer with a passion for learning.
             <br className="sm:block hidden" /> I develop projects using multiple
             technologies, here are some of my favorites!
           </p>
@@ -25,6 +25,24 @@ const Hero = () => {
       </div>
 
       <ComputersCanvas />
+
+      <div className="absolute xs:bottom-0.5 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.dev
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
