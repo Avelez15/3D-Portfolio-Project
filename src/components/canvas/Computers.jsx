25cd58681fc6fc5360f202ecd963c423b78d1db0
Alejandro Velez <velez.alejandro4@gmail.com>
2024-05-02 22:49:03 -0400

    got 3d model to show

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,4 +1,4 @@
-import { Suspense, useEffect, useState } from "react";
+import react, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
@@ -11,7 +11,12 @@ const Computers = () => {
     <mesh>
       <hemisphereLight intensity={0.15} groundColor="black" />
       <pointLight intensity={1} />
-      <primitive object={computer.scene} />
+      <primitive
+        object={computer.scene}
+        scale={0.75}
+        position={[0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
     </mesh>
   );
 };
@@ -38,4 +43,4 @@ const ComputersCanvas = () => {
   );
 };
 
-export default Computers;
+export default ComputersCanvas;
